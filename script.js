53472be9acf3ee3e902d4bf80f6dbd5fb0a2efa8
Muhammad Arif <arif1951995@gmail.com>
2019-07-05 05:44:59 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ myRequest.open('GET', 'https://cors-anywhere.herokuapp.com/https://tk-whatsapp.h
 myRequest.send();
 myRequest.onload = () => {
     let data =     JSON.parse(myRequest.responseText);   
-     console.table(data);
+   console.log(data);
      data.forEach((e,i) => {
         // console.table(e);
       
         if(e.numbUnread > 0) {
             document.querySelector('ul').insertAdjacentHTML('afterbegin', `
-        <li id='unread-li'> <div class="left" class="img"> <img src="${e.profilePic}" alt="Profile Pic"> </div><div class="center"> <div class="heading"> <h3>${e.name}</h3> <p>${e.number}</p></div><div class="message"> <p>${e.firstLine}</p></div><div class="time"> 12:31 PM today </div></div><div id="unread-div" class="right"> <span class="numOfUnreadMessage">${e.numbUnread}</span> </div></li>
+        <li id='unread-li'> <div class="left" class="img"> <img src="${e.profilePic}" alt="Profile Pic"> </div><div class="center"> <div class="heading"> <h3>${e.name}</h3> <p>${e.number}</p></div><div class="message"> <p>${e.firstLine}</p></div><div class="time"> ${e.time} </div></div><div id="unread-div" class="right"> <span class="numOfUnreadMessage">${e.numbUnread}</span> </div></li>
         `);
         }else {
             document.querySelector('ul').insertAdjacentHTML('afterbegin', `
-            <li> <div class="left" class="img"> <img src="${e.profilePic}" alt="Profile Pic"> </div><div class="center"> <div class="heading"> <h3>${e.name}</h3> <p>${e.number}</p></div><div class="message"> <p>${e.firstLine}</p></div><div class="time"> 12:31 PM today </div></div><div class="right"> <span class="numOfUnreadMessage">${e.numbUnread}</span> </div></li>
+            <li> <div class="left" class="img"> <img src="${e.profilePic}" alt="Profile Pic"> </div><div class="center"> <div class="heading"> <h3>${e.name}</h3> <p>${e.number}</p></div><div class="message"> <p>${e.firstLine}</p></div><div class="time"> ${e.time} </div></div><div class="right"> <span class="numOfUnreadMessage">${e.numbUnread}</span> </div></li>
             `);
         }
     });
