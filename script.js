780828f6d0313d2b2f51332b92887b1df60e97aa
Muhammad Arif <arif1951995@gmail.com>
2019-07-06 00:44:35 -0700

    changed xhtmlhttpRequest with fetch()

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-let myRequest = new XMLHttpRequest();
-myRequest.open('GET', 'https://cors-anywhere.herokuapp.com/https://tk-whatsapp.herokuapp.com/messages');
-myRequest.send();
-myRequest.onload = () => {
-    let data =     JSON.parse(myRequest.responseText);   
-   console.log(data);
-     data.forEach((e,i) => {
-        // console.table(e);
-      
+fetch(`https://cors-anywhere.herokuapp.com/https://tk-whatsapp.herokuapp.com/messages`)
+.then(result => {
+    console.log(result);
+    return result.json();
+})
+.then(data => {
+    console.log(data);
+    data.forEach(e => {
         if(e.numbUnread > 0) {
             document.querySelector('ul').insertAdjacentHTML('afterbegin', `
         <li id='unread-li'> <div class="left" class="img"> <img src="${e.profilePic}" alt="Profile Pic"> </div><div class="center"> <div class="heading"> <h3>${e.name}</h3> <p>${e.number}</p></div><div class="message"> <p>${e.firstLine}</p></div><div class="time"> ${e.time} </div></div><div id="unread-div" class="right"> <span class="numOfUnreadMessage">${e.numbUnread}</span> </div></li>
@@ -16,8 +15,11 @@ myRequest.onload = () => {
             <li> <div class="left" class="img"> <img src="${e.profilePic}" alt="Profile Pic"> </div><div class="center"> <div class="heading"> <h3>${e.name}</h3> <p>${e.number}</p></div><div class="message"> <p>${e.firstLine}</p></div><div class="time"> ${e.time} </div></div><div class="right"> <span class="numOfUnreadMessage">${e.numbUnread}</span> </div></li>
             `);
         }
-    });
-    
-}
 
+   
+    });
+})
+.catch(error => {
+    document.write(`<h1>${error}</h1>`)
+})
 
