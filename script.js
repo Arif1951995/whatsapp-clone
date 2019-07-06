2c8cf1272bb850af35d289a3d5e9c1b0f943d362
Muhammad Arif <arif1951995@gmail.com>
2019-07-06 00:46:26 -0700

    added indentations in js file

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 fetch(`https://cors-anywhere.herokuapp.com/https://tk-whatsapp.herokuapp.com/messages`)
 .then(result => {
-    console.log(result);
     return result.json();
 })
 .then(data => {
-    console.log(data);
     data.forEach(e => {
         if(e.numbUnread > 0) {
             document.querySelector('ul').insertAdjacentHTML('afterbegin', `
@@ -15,8 +13,6 @@ fetch(`https://cors-anywhere.herokuapp.com/https://tk-whatsapp.herokuapp.com/mes
             <li> <div class="left" class="img"> <img src="${e.profilePic}" alt="Profile Pic"> </div><div class="center"> <div class="heading"> <h3>${e.name}</h3> <p>${e.number}</p></div><div class="message"> <p>${e.firstLine}</p></div><div class="time"> ${e.time} </div></div><div class="right"> <span class="numOfUnreadMessage">${e.numbUnread}</span> </div></li>
             `);
         }
-
-   
     });
 })
 .catch(error => {
