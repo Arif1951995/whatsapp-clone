013b687e141980410ba9df5e9b521fea0245c4ab
Muhammad Arif <arif1951995@gmail.com>
2019-07-06 01:24:31 -0700

    used async/await instead of fetch()

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-fetch(`https://cors-anywhere.herokuapp.com/https://tk-whatsapp.herokuapp.com/messages`)
-.then(result => {
-    return result.json();
-})
-.then(data => {
-    data.forEach(e => {
-        console.log(e);
-        if(e.isUnread) {
-            document.querySelector('ul').insertAdjacentHTML('afterbegin', `
-        <li id='unread-li'> <div class="left" class="img"> <img src="${e.profilePic}" alt="Profile Pic"> </div><div class="center"> <div class="heading"> <h3>${e.name}</h3> <p>${e.number}</p></div><div class="message"> <p>${e.firstLine}</p></div><div class="time"> ${e.time} </div></div><div id="unread-div" class="right"> <span class="numOfUnreadMessage">${e.numbUnread}</span> </div></li>
-        `);
-        }else {
-            document.querySelector('ul').insertAdjacentHTML('afterbegin', `
-            <li> <div class="left" class="img"> <img src="${e.profilePic}" alt="Profile Pic"> </div><div class="center"> <div class="heading"> <h3>${e.name}</h3> <p>${e.number}</p></div><div class="message"> <p>${e.firstLine}</p></div><div class="time"> ${e.time} </div></div><div class="right"> <span class="numOfUnreadMessage">${e.numbUnread}</span> </div></li>
-            `);
+
+/************
+used fetch to complete and then aync/await
+both work fine seprately
+*/ 
+
+
+// fetch(`https://cors-anywhere.herokuapp.com/https://tk-whatsapp.herokuapp.com/messages`)
+// .then(result => {
+//     return result.json();
+// })
+// .then(data => {
+//     data.forEach(e => {
+//         if(e.isUnread) {
+//             document.querySelector('ul').insertAdjacentHTML('afterbegin', `
+//         <li id='unread-li'> <div class="left" class="img"> <img src="${e.profilePic}" alt="Profile Pic"> </div><div class="center"> <div class="heading"> <h3>${e.name}</h3> <p>${e.number}</p></div><div class="message"> <p>${e.firstLine}</p></div><div class="time"> ${e.time} </div></div><div id="unread-div" class="right"> <span class="numOfUnreadMessage">${e.numbUnread}</span> </div></li>
+//         `);
+//         }else {
+//             document.querySelector('ul').insertAdjacentHTML('afterbegin', `
+//             <li> <div class="left" class="img"> <img src="${e.profilePic}" alt="Profile Pic"> </div><div class="center"> <div class="heading"> <h3>${e.name}</h3> <p>${e.number}</p></div><div class="message"> <p>${e.firstLine}</p></div><div class="time"> ${e.time} </div></div><div class="right"> <span class="numOfUnreadMessage">${e.numbUnread}</span> </div></li>
+//             `);
+//         }
+//     });
+// })
+// .catch(error => {
+//     document.write(`<h1>${error}</h1>`)
+// })
+
+
+
+
+async function getDataAW() {
+        try {
+            let result = await fetch(` https://cors-anywhere.herokuapp.com/https://tk-whatsapp.herokuapp.com/messages`);
+            let data = await result.json();
+            data.forEach(e => {
+                if(e.isUnread) {
+                    document.querySelector('ul').insertAdjacentHTML('afterbegin', `
+                <li id='unread-li'> <div class="left" class="img"> <img src="${e.profilePic}" alt="Profile Pic"> </div><div class="center"> <div class="heading"> <h3>${e.name}</h3> <p>${e.number}</p></div><div class="message"> <p>${e.firstLine}</p></div><div class="time"> ${e.time} </div></div><div id="unread-div" class="right"> <span class="numOfUnreadMessage">${e.numbUnread}</span> </div></li>
+                `);
+                }else {
+                    document.querySelector('ul').insertAdjacentHTML('afterbegin', `
+                    <li> <div class="left" class="img"> <img src="${e.profilePic}" alt="Profile Pic"> </div><div class="center"> <div class="heading"> <h3>${e.name}</h3> <p>${e.number}</p></div><div class="message"> <p>${e.firstLine}</p></div><div class="time"> ${e.time} </div></div><div class="right"> <span class="numOfUnreadMessage">${e.numbUnread}</span> </div></li>
+                    `);
+                }
+            });
+        }catch(error) {
+            document.write(`<h1>${error}</h1>`)
         }
-    });
-})
-.catch(error => {
-    document.write(`<h1>${error}</h1>`)
-})
+
+
+   
+
+}
+
+getDataAW();
 
 
