e1cc6c931b5ca6d443a4b275cb16368a52fa9acb
Muhammad Arif <arif1951995@gmail.com>
2019-07-06 00:55:03 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ fetch(`https://cors-anywhere.herokuapp.com/https://tk-whatsapp.herokuapp.com/mes
 })
 .then(data => {
     data.forEach(e => {
-        if(e.numbUnread > 0) {
+        console.log(e);
+        if(e.isUnread) {
             document.querySelector('ul').insertAdjacentHTML('afterbegin', `
         <li id='unread-li'> <div class="left" class="img"> <img src="${e.profilePic}" alt="Profile Pic"> </div><div class="center"> <div class="heading"> <h3>${e.name}</h3> <p>${e.number}</p></div><div class="message"> <p>${e.firstLine}</p></div><div class="time"> ${e.time} </div></div><div id="unread-div" class="right"> <span class="numOfUnreadMessage">${e.numbUnread}</span> </div></li>
         `);
@@ -18,4 +19,5 @@ fetch(`https://cors-anywhere.herokuapp.com/https://tk-whatsapp.herokuapp.com/mes
 .catch(error => {
     document.write(`<h1>${error}</h1>`)
 })
+
 
